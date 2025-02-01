e4ad32c2b17dc6aef5f243bba7e9327753b09ce3
theo-mandelbaum <mandelbaumtheo@gmail.com>
2025-01-31 20:36:29 -0500

    chapter 3 exercise 2 complete

--- software-design-by-example/python/chapter-3-exercises/chap3.py
+++ software-design-by-example/python/chapter-3-exercises/chap3.py
@@ -5,22 +5,34 @@
 # and rewrite the duplicate finder from this chapter to use it.
 
 import sys
-import hashlib
+from hashlib import sha256
 
 def same_bytes(left_name, right_name):
     left_bytes = open(left_name, "rb").read()
     right_bytes = open(right_name, "rb").read()
     return left_bytes == right_bytes
 
+def find_groups(filenames):
+    groups = {}
+    for fn in filenames:
+        data = open(fn, "rb").read()
+        hash_code = sha256().update(data)
+        if hash_code not in groups:
+            groups[hash_code] = set()
+        groups[hash_code].add(fn)
+    return groups
+
 def find_duplicates(filenames):
     matches = []
     for left in filenames:
         for right in filenames:
-            if same_bytes(left, right):
+            if left != right and same_bytes(left, right):
                 matches.append((left, right))
     return matches
 
 if __name__ == "__main__":
-    duplicates = find_duplicates(sys.argv[1:])
-    for (left, right) in duplicates:
-        print(left, right)
+    groups = find_groups(sys.argv[1:])
+    for group in groups.values():
+        duplicates = find_duplicates(list(group))
+        for left, right in duplicates:
+            print(left, right)
